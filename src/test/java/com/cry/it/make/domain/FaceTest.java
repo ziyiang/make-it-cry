ba32aa215fe4b81534aeaad298c74619389c6434
Shuai Wang <shuai.wang1@thoughtworks.com>
2020-04-16 09:51:16 +0800

    correct something

--- src/test/java/com/cry/it/make/domain/FaceTest.java
+++ src/test/java/com/cry/it/make/domain/FaceTest.java
@@ -9,6 +9,6 @@ public class FaceTest {
 
     @Test
     public void should_smile() throws Exception {
-        assertThat(new Face().face(), is("cry"));
+        assertThat(new Face().face(), is("smile"));
     }
 }
